3e7c557eb6adb932703d328fb9b075dba15c58a2
Soichi Hayashi <hayashis@iu.edu>
2021-03-13 04:11:11 +0000

    finally got jupyterlab-git to install

--- containers/dipy/build.sh
+++ containers/dipy/build.sh
@@ -1,5 +1,8 @@
+docker pull jupyter/datascience-notebook:lab-2.1.1
+
 set -e
 set -x
+
 docker build -t brainlife/ga-dipy .
-docker tag brainlife/ga-dipy brainlife/ga-dipy:1.0
+docker tag brainlife/ga-dipy brainlife/ga-dipy:lab211-dipy130
 docker push brainlife/ga-dipy
