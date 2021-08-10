ba841f51267f65d9ce4ab807fc61fb0d3e5b6ea0
Soichi Hayashi <soichih@gmail.com>
2021-08-10 09:04:02 -0400

    commiting some old changes we made but never pushed

--- containers/dipy/build.sh
+++ containers/dipy/build.sh
@@ -1,8 +1,7 @@
-docker pull jupyter/datascience-notebook:lab-2.1.1
 
 set -e
 set -x
 
 docker build -t brainlife/ga-dipy .
-docker tag brainlife/ga-dipy brainlife/ga-dipy:lab211-dipy130
+docker tag brainlife/ga-dipy brainlife/ga-dipy:lab3016-dipy130
 docker push brainlife/ga-dipy
